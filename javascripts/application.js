7f783ab5d48fc7ccca2f99595af005e3fd744076
Billy Kimble <basslines@gmail.com>
2013-01-31 16:13:58 -0800

    updated it to be closer to pixel perfection. Got twitter button working. Fixed Firebug rendering issue

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,17 +1,22 @@
 $(function() {
 
-	$(".enter_button").hover(function(e){
-		$(this).toggleClass("enter_button_hover");
-	});
-	$(".fb_button").hover(function(e){
-		$(this).toggleClass("fb_button_hover");
-	});
-	$(".twitter_button").hover(function(e){
-		$(this).toggleClass("twitter_button_hover");
-	});
-
-	$(".page_scroll").scrollable({
-		prev: ".page_prev",
-		next: ".page_next"
-	});
+  $(".enter_button").hover(function(){
+    $(this).toggleClass("enter_button_hover");
+  });
+  $(".fb_button").hover(function(){
+    $(this).toggleClass("fb_button_hover");
+  });
+  $(".twitter_button").hover(function(){
+    $(this).toggleClass("twitter_button_hover");
+  });
+  $(".arrow_right").hover(function(){
+    $(this).toggleClass("arrow_right_hover");
+  });
+  $(".arrow_left").hover(function(){
+    $(this).toggleClass("arrow_left_hover");
+  });
+  $(".page_scroll").scrollable({
+    prev: ".page_prev",
+    next: ".page_next"
+  });
 });
